ca278c01515f6ca186db82b7b831aa9803db608e
Armando Fox <fox@a1patronsystems.com>
2008-08-09 20:19:12 +0000

    *** empty log message ***

--- app/helpers/options_helper.rb
+++ app/helpers/options_helper.rb
@@ -1,2 +1,15 @@
 module OptionsHelper
+
+  def edit_field_for(v)
+    case v.typ
+    when :int
+      text_field_tag "values[#{v.name}]", v.value, :size => 6, :maxlength => 6
+    when :string
+      text_field_tag "values[#{v.name}]", v.value, :size => 60
+    when :text
+      text_area_tag "values[#{v.name}]", v.value, :rows => 6, :cols => 60
+    else
+      text_field_tag "values[#{v.name}]", v.value, :size => 6, :maxlength => 6
+    end
+  end
 end
