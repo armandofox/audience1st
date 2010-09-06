33fdfaf9fbdc5fe31bb817f5d7b09b5ba1ac0e67
Armando Fox <fox@a1patronsystems.com>
2010-09-06 22:00:15 +0000

    typo in new_customers report, created_on should be created_at

--- app/models/reports/new_customers.rb
+++ app/models/reports/new_customers.rb
@@ -7,7 +7,7 @@ class NewCustomers < Report
     from = Time.from_param(params[:newFrom])
     to = Time.from_param(params[:newTo])
     from,to = to,from if from > to
-    conds = 'created_on BETWEEN ? AND ?'
+    conds = 'created_at BETWEEN ? AND ?'
     conds << " AND email LIKE '%@%'" if params[:require_valid_email]
     conds << ' AND street IS NOT NULL' if params[:require_valid_address]
     @customers = Customer.find(:all, :conditions => [conds, from, to],
