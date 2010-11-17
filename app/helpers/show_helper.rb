8cce231c4919a3a16ff9df2ac7658bd177e40a8d
Armando Fox <fox@a1patronsystems.com>
2010-11-17 23:33:54 +0000

    Fixed link helpers (and moved them to show_helper.rb) for shows and showdates so that links for Special events use the correct Store#special action.  Modified RSS feed to use the helpers (formerly had hardcoded url_for).  Ideally, RESTful route should encapsulate whether the resource is special or not, and url_for should just work.

--- app/helpers/show_helper.rb
+++ app/helpers/show_helper.rb
@@ -1,2 +1,15 @@
 module ShowHelper
+
+  def link_to_show_tickets(show, args={})
+    action = (show.special? ? 'special' : 'index')
+    url_for({:only_path => false, :controller => 'store', :action => action,
+        :show_id => show.id}.merge(args))
+  end
+
+  def link_to_showdate_tickets(showdate, args={})
+    action = (showdate.show.special? ? 'special' : 'index')
+    url_for({:only_path => false, :controller => 'store', :action => action,
+        :showdate_id => showdate.id}.merge(args))
+  end
+
 end
