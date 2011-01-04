5ff1565da28fbb839e0a2e5431b1289c1f3312b2
Armando Fox <fox@a1patronsystems.com>
2011-01-04 20:03:01 +0000

    split season calculations into 2 migrations to be nondestructive

--- db/migrate/20101228225607_add_season.rb
+++ db/migrate/20101228225607_add_season.rb
@@ -6,22 +6,17 @@ class AddSeason < ActiveRecord::Migration
         season = Regexp.last_match(0)
       elsif v.expiration_date.year <= 2011
         season = v.expiration_date.year
-      elsif v.sold_on.year <= 2011
-        season = v.sold_on.year
+      elsif v.created_on.year <= 2011
+        season = v.created_on.year
       else
         season = 2011
       end
       v.update_attribute(:season, season)
     end
-    remove_column :vouchertypes, :expiration_date
     rename_column :vouchertypes, :created_on, :created_at
   end
 
   def self.down
-    add_column :vouchertypes, :expiration_date, :datetime, :null => true, :default => nil
-    Vouchertype.find(:all).each do |v|
-      v.update_attribute(:expiration_date, Date.civil(v.season, 12, 31))
-    end
     remove_column :vouchertypes, :season
     rename_column :vouchertypes, :created_at, :created_on
   end
