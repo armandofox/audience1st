ab3e8dbeb2404a94e1e53b3cab35f3e1461ba51a
Armando Fox <fox@a1patronsystems.com>
2011-02-04 01:25:58 +0000

    modifeid migration to mark all vouchers for showdates before migration date as used

--- db/migrate/20110203013026_checkin.rb
+++ db/migrate/20110203013026_checkin.rb
@@ -1,6 +1,7 @@
 class Checkin < ActiveRecord::Migration
   def self.up
     rename_column 'vouchers', 'used', 'checked_in'
+    connection.execute("UPDATE vouchers v JOIN showdates s ON v.showdate_id=s.id SET v.checked_in=1 WHERE s.thedate <= '#{Time.now.to_formatted_s(:db)}'")
   end
 
   def self.down
