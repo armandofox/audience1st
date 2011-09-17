183000ab742dcf3bc9a44f2ca834cdc6fbb9c1a0
Armando Fox <fox@cs.berkeley.edu>
2011-09-17 16:56:09 -0700

    moved stripe keys config stuff into admin-visible part of options config

--- db/migrate/20110916232317_add_stripe_publishable_key.rb
+++ db/migrate/20110916232317_add_stripe_publishable_key.rb
@@ -2,9 +2,9 @@ class AddStripePublishableKey < ActiveRecord::Migration
   require 'add_option_helper'
   def self.up
     AddOptionHelper.add_new_option(
-      20, 'Config', 'stripe_publishable_key', '', :string)
+      4003, 'External Integration', 'stripe_publishable_key', '', :string)
     AddOptionHelper.add_new_option(
-      21, 'Config', 'stripe_secret_key', '', :string)
+      4004, 'External Integration', 'stripe_secret_key', '', :string)
     # and delete some obsolete options
     %w(monthly_fee cc_fee_markup per_ticket_fee per_ticket_commission customer_service_per_hour).each do |k|
       if (o = Option.find_by_name(k))
