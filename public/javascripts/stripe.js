8830d96ed9507af28f60247af956b241227f52aa
Armando Fox <fox@cs.berkeley.edu>
2011-09-18 13:10:43 -0700

    Trying to look at merging the two payment flows (store checkout and walkup sale) to allow admins to swipe and/or enter check/cash payments on either flow with same UI.  Going to do a branch to experiment w/this.

--- public/javascripts/stripe.js
+++ public/javascripts/stripe.js
@@ -1,19 +1,26 @@
+// Functions for handling payments through Stripe.
+// These make the following assumptions about available form fields:
+//  #_stripe_api_key       API key for stripe calls
+//  ._stripe_total         floating-point total field; any characters
+//                            that are not a digit or . are squeezed out
+//  #_stripe_payment_form  form to submit to server
+//  #_stripe_submit        the form's submit button (for enabling/disabling)
+//  #credit_card_token    hidden field that will carry token returned by Stripe
+//  #payment_errors        element in which to display errors from API call
+
 function getStripeTotal() {
-  return(100 * Number($$('._stripe_total')[0].innerHTML.substring(1)));
-}
-
-function getStripeDescription() {
-
+  var total = $$('._stripe_total')[0].innerHTML;
+  total = total.replace(/[^0-9.]+/g, '');
+  return(100 * Number(total));
 }
 
 function stripeResponseHandler(status, response) {
   if (response.error) {
     // re-enable submit button
     $('_stripe_submit').disabled = false;
-    $('payment_errors').innerHTML = 'Please correct the following problems:<br/>' + 
-      response.error.message;
+    $('payment_errors').innerHTML = 'Please correct the following problems:<br/>' +   response.error.message;
   } else {
-    $('_stripe_payment_form')['credit_card_token'].setValue(response['id']);
+    $('credit_card_token').setValue(response['id']);
     $('_stripe_payment_form').submit();
   }
 }
@@ -24,26 +31,25 @@ function stripeResponseHandler(status, response) {
 // Submit button's ID is _stripe_submit
 
 function stripeSubmit(event) {
-  // clear out errors field
-  $('payment_errors').innerHTML = '';
-  // disable submit button
-  $('_stripe_submit').disabled = true;
-  var name = $$('#billing #customer_first_name')[0].getValue() + ' ' + 
-    $$('#billing #customer_last_name')[0].getValue();
+  Stripe.setPublishableKey($('_stripe_api_key').getValue());
+  $('payment_errors').innerHTML = '';  //  clear out errors field
+  $('_stripe_submit').disabled = true; // disable submit button
   var total = getStripeTotal();
-  Stripe.createToken({
-      number: $('credit_card_number').getValue(),
+  var card = {
+        number: $('credit_card_number').getValue(),
         cvc: $('credit_card_verification_value').getValue(),
         exp_month: $('credit_card_month').getValue(),
-        exp_year: $('credit_card_year').getValue(),
-        name:  name,
-        address_line1: $$('#billing #customer_street')[0].getValue(),
-        address_zip: $$('#billing #customer_zip')[0].getValue(),
-        address_state: $$('#billing #customer_state')[0].getValue()
-        },
-    total,
-    stripeResponseHandler);
-  return false;
+        exp_year: $('credit_card_year').getValue()
+  };
+  if ($('billing')) {             // billing name/addr available on form?
+      card.name = $$('#billing #customer_first_name')[0].getValue() + ' ' + 
+        $$('#billing #customer_last_name')[0].getValue();
+      card.address_line1 = $$('#billing #customer_street')[0].getValue();
+      card.address_zip = $$('#billing #customer_zip')[0].getValue();
+      card.address_state = $$('#billing #customer_state')[0].getValue();
+  }
+  Stripe.createToken(card, total, stripeResponseHandler);
+  return(false);
 }
 
 
