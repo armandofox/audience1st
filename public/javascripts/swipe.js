7d9785845e77b963430fef409e9543ff8a77c7b5
Armando Fox <fox@cs.berkeley.edu>
2011-09-22 15:33:52 -0700

    removed unnecessary collectKeys function, trimmed whitespace from cardholder first/last name in parseSwipeData

--- public/javascripts/swipe.js
+++ public/javascripts/swipe.js
@@ -4,7 +4,6 @@
 // after 10 secs, go back to original state.
 function waitForSwipe() {
     var timeout = 8;            // in seconds
-    document.onKeyPress = collectKeys;
     $('ccReady').style.display = 'none';
     $('ccWaiting').style.display = 'block';
     $('swipe_data').value = '';
@@ -16,16 +15,6 @@ function resetSwipe() {
     $('ccReady').style.display = 'block';
     $('credit_card_verification_value').focus();
 }
-// Ignore Enter/CR (which would normally sbmit form)
-function collectKeys(evt) {
-    var evt = (evt) ? evt : ((event) ? event : null);
-    var node = (evt.target) ? evt.target : ((evt.srcElement) ? evt.srcElement : null); 
-    if (evt.keyCode == 13)  { 
-        parseSwipeData();
-        stripeSubmit(evt);
-        resetSwipe();
-    }
-}
 
 // convert swipe data to form fields
 function parseSwipeData() {
@@ -34,21 +23,21 @@ function parseSwipeData() {
     var trk2 = /;(\d{1,19})=(\d\d)(\d\d).{3,12}\?/;
     var elts;
     if (elts = swipe.match(trk1)) {
-        $('credit_card_number').setValue(elts[1]);
-        $('credit_card_last_name').setValue(elts[2]);
-        $('credit_card_first_name').setValue(elts[3]);
-        setSelectedYear('credit_card_year', 2000+Number(elts[4]));
-        $('credit_card_month').selectedIndex = Number(elts[5]) - 1;
+      $('credit_card_number').setValue(elts[1]);
+      $('credit_card_last_name').setValue(elts[2].replace(/^\s+|\s+$/g, ''));
+      $('credit_card_first_name').setValue(elts[3].replace(/^\s+|\s+$/g, ''));
+      setSelectedYear('credit_card_year', 2000+Number(elts[4]));
+      $('credit_card_month').selectedIndex = Number(elts[5]) - 1;
     } else if (elts = swipe.match(trk2)) {
-        $('credit_card_number').setValue(elts[1]);
-        setSelectedYear('credit_card_year', 2000+Number(elts[2]));
-        $('credit_card_month').selectedIndex = Number(elts[3]) - 1;
-        $('credit_card_last_name').setValue('');
-        $('credit_card_first_name').setValue('');
+      $('credit_card_number').setValue(elts[1]);
+      setSelectedYear('credit_card_year', 2000+Number(elts[2]));
+      $('credit_card_month').selectedIndex = Number(elts[3]) - 1;
+      $('credit_card_last_name').setValue('');
+      $('credit_card_first_name').setValue('');
     } else {
-        $('credit_card_number').setValue('ERROR');
-        $('credit_card_last_name').setValue('ERROR');
-        $('credit_card_first_name').setValue('ERROR');
+      $('credit_card_number').setValue('ERROR');
+      $('credit_card_last_name').setValue('ERROR');
+      $('credit_card_first_name').setValue('ERROR');
     }
     resetSwipe();
     return false;
