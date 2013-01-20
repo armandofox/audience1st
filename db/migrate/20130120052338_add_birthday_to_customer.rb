3b755a5745ea9cf39a3de48acc8d9ee028c36c7f
Armando Fox <fox@cs.berkeley.edu>
2013-01-20 15:36:32 -0800

    removed spurious dup migration for birtdays

--- db/migrate/20130120052338_add_birthday_to_customer.rb
+++
@@ -1,8 +0,0 @@
-class AddBirthdayToCustomer < ActiveRecord::Migration
-  def self.up
-    add_column :customers, :birthday, :datetime, :null => true, :default => nil
-  end
-
-  def self.down
-  end
-end
