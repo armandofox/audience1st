6c088d816a4600fb681f72ededb50bad8ec9f340
Armando Fox <fox@cs.berkeley.edu>
2014-12-16 21:05:55 -0800

    all scenarios passing big merge

--- features/step_definitions/report_steps.rb
+++ features/step_definitions/report_steps.rb
@@ -18,7 +18,7 @@ When /^I run the accounting report from "(.*)" to "(.*)"$/ do |from,to|
   steps %Q{
   When I visit the reports page
   And I select "Earned Revenue" from "Report type"
-  And I select "#{Time.parse(from).to_formatted_s}" as the "From:" date
+  And I select "#{Time.parse(from).to_formatted_s}" as the "Or custom range from:" date
   And I select "#{Time.parse(to).to_formatted_s}" as the "To:" date
   And I press "Go"
 }
