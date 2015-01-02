5ea0481a718f6e31181a034429ac4fb327af5b54
Armando Fox <fox@cs.berkeley.edu>
2015-01-01 19:32:46 -0800

    much nicer popup help

--- app/helpers/popup_help_helper.rb
+++ app/helpers/popup_help_helper.rb
@@ -2,8 +2,7 @@ module PopupHelpHelper
 
   def popup_help_for(item)
     unless (defined?(for_email)) || (m = POPUP_HELP[item.to_sym]).blank?
-      link_to_function "What's this?", "alert('#{escape_javascript(m)}')", {:class => 'popupHelpLink'}
-      # content_tag :a, "What's this?", :title => m.gsub(/\s+/m, ' ').wrap, :class => 'popupHelpLink'
+      render :partial => 'messages/popup_help', :object => m
     end
   end
 
