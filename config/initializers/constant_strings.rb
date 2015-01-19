18449a0d2e76662a0d6fc38bf015a02e16eef9f3
Armando Fox <fox@cs.berkeley.edu>
2015-01-18 23:50:42 -0800

    removing more obsolete stuff

--- config/initializers/constant_strings.rb
+++ config/initializers/constant_strings.rb
@@ -5,6 +5,5 @@ end
 POPUP_HELP =          read_yaml_file 'popup_help'
 OPTION_DESCRIPTIONS = read_yaml_file 'option_descriptions'
 APP_CONFIG =          read_yaml_file 'settings'
-MESSAGES = APP_CONFIG[:messages].symbolize_keys
 ENV['TZ'] = APP_CONFIG[:timezone] || 'PST8PDT' # local timezone
 APP_VERSION = IO.read("#{RAILS_ROOT}/REVISION").to_s.strip rescue "DEV"
