70e8bdd57041a17aa109280ee588edbe69f3c03d
Armando Fox <fox@cs.berkeley.edu>
2015-06-07 13:55:52 -0700

    progress towards feature

--- db/migrate/20150603065815_per_order_service_charge.rb
+++ db/migrate/20150603065815_per_order_service_charge.rb
@@ -1,11 +1,14 @@
 class PerOrderServiceCharge < ActiveRecord::Migration
   def self.up
     add_column :options, :subscription_order_service_charge, :integer, :default => 0
-    add_column :options, :subscription_service_charge_description, :string, :null => true, :default => nil
-    add_column :options, :subscription_service_charge_account_code, :string, :null => true, :default => nil
+    add_column :options, :subscription_order_service_charge_description, :string, :null => true, :default => nil
+    add_column :options, :subscription_order_service_charge_account_code, :string, :null => true, :default => nil
     add_column :options, :regular_order_service_charge, :integer, :default => 0
-    add_column :options, :regular_service_charge_description, :string, :null => true, :default => nil
-    add_column :options, :regular_service_charge_account_code, :string, :null => true, :default => nil
+    add_column :options, :regular_order_service_charge_description, :string, :null => true, :default => nil
+    add_column :options, :regular_order_service_charge_account_code, :string, :null => true, :default => nil
+    add_column :options, :classes_order_service_charge, :integer, :default => 0
+    add_column :options, :classes_order_service_charge_description, :string, :null => true, :default => nil
+    add_column :options, :classes_order_service_charge_account_code, :string, :null => true, :default => nil
   end
 
   def self.down
