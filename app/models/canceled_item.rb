b9929be989751b17b5ea04f3fa6122e8a0a7f207
Armando Fox <fox@cs.berkeley.edu>
2015-06-12 19:25:31 -0700

    forgot add file

---
+++ app/models/canceled_item.rb
@@ -0,0 +1,14 @@
+class CanceledItem < Item
+  belongs_to :account_code
+
+  def price ; 0 ; end
+  def amount ; 0 ; end
+
+  def cancel!(by_whom) ; end # overridden from parent class
+
+  def one_line_description ; attributes['comments'] ; end
+  def description_for_audit_txn ; attributes['comments'] ; end
+  def comments ; '' ; end
+
+  def cancelable? ; false ;  end
+end
