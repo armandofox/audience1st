4f56d6de724b37040a2725e8e26579e973b604d2
Armando Fox <fox@cs.berkeley.edu>
2015-08-08 18:47:36 -0700

    Now get a reasonable error message (not exception) if a customer has no orders and you try to view order history

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -4,7 +4,8 @@ class OrdersController < ApplicationController
   def index
     redirect_with(root_path, :alert => 'No customer specified') and return unless
       @customer = Customer.find_by_id(params[:customer_id])
-    @orders = @customer.orders
+    redirect_with(root_path, :notice => "#{@customer.full_name} has no orders.') and return if
+      (@orders = @customer.orders).empty?
     render :partial => 'order', :collection => @orders, :layout => true
   end
 
