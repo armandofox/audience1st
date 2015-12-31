98d39c21f285f8e6ae42e56c7031363e95aa36dd
Armando Fox <fox@cs.berkeley.edu>
2015-12-30 16:52:39 -0800

    order comments now appear on door list as part of voucher comments field

--- app/helpers/vouchers_helper.rb
+++ app/helpers/vouchers_helper.rb
@@ -1,6 +1,8 @@
 module VouchersHelper
 
   def comments_for_voucherlist(vouchers)
-    vouchers.map { |v| v.comments unless v.comments.blank? }.compact.uniq.join(";")
+    comments = [ vouchers.first.order.comments ]
+    comments += vouchers.map { |v| v.comments unless v.comments.blank? }
+    comments.compact.uniq.join(";")
   end
 end
