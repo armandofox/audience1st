789de196fa18f1e4fbd9ab1c65e4ecbf01f69cc7
Armando Fox <fox@cs.berkeley.edu>
2016-07-02 17:19:27 -0700

    refactored and fixed customer quicksearch

--- public/javascripts/customer_search.js
+++ public/javascripts/customer_search.js
@@ -1,25 +1,36 @@
-A1.autocomplete = {
-  textField: '#autocomplete',
-  idField:   '#id',
-  url: '/ajax/customer_autocomplete'
-};
+A1.autocomplete_url = '/ajax/customer_autocomplete';
+A1.autocomplete_selector = '._autocomplete';
 
-A1.select_search_result = function(event, customer) {
+A1.select_search_result = function(customer,textField,idField) {
   // User has selected a customer using autocomplete.
   // The customer object has label and value properties, where value is
   //   the customer id and label (in our case) is the customer full name.
   // Populate the '#id' field with the id, and the autocomplete text field
   //   with the chosen name.
-  jQuery(A1.autocomplete.textField).val(customer.item.label);
-  jQuery(A1.autocomplete.idField).val(customer.item.value);
-  return(false);
+  textField.val(customer.item.label);
+  idField.val(customer.item.value);
 };
 
-      
-jQuery(function() {
-  jQuery(A1.autocomplete.textField).autocomplete({
-    source: A1.autocomplete.url,
-    minLength: 2,
-    select: A1.select_search_result
+A1.setup_autocomplete_fields = function() {
+  jQuery(A1.autocomplete_selector).each(function(i,elt) {
+    var e = jQuery(elt);
+    // which ID field is associated with this autocomplete element?
+    var idField = jQuery('#' + e.data('resultfield'));
+    // Blank out the ID field when search box gets focus
+    e.focus(function(e) { idField.val(''); });
+    // turn off browser autocompletion for text box
+    e.attr('autocomplete', 'off');
+    // set up autocompletion
+    e.autocomplete({
+      source: A1.autocomplete_url,
+      minLength: 2,
+      select: function(event, selection) { 
+        A1.select_search_result(selection,e,idField);   
+        return(false);
+      }
+    });
   });
-});
+};
+
+jQuery(A1.setup_autocomplete_fields);
+
