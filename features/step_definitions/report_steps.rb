40057add7775615960f4aaf7ed8431d2f09e5b77
Armando Fox <fox@cs.berkeley.edu>
2016-07-19 16:45:39 -0700

    fixed js in browse customer reports feature

--- features/step_definitions/report_steps.rb
+++ features/step_definitions/report_steps.rb
@@ -20,8 +20,7 @@ When /^I run the accounting report from "(.*)" to "(.*)"$/ do |from,to|
   steps %Q{
   When I visit the reports page
   And I select "Earned Revenue" from "Report type"
-  And I select "#{Time.parse(from).to_formatted_s}" as the "Or custom range from:" date
-  And I select "#{Time.parse(to).to_formatted_s}" as the "To:" date
+  And I select "#{from} to #{to}" as the "report_dates" date range
   And I press "Display report"
 }
 end
