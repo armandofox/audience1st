82b28042c278358b1e512a74196912e257b03600
Armando Fox <fox@cs.berkeley.edu>
2016-07-23 21:22:24 -0700

    hotfix to allow max_sales_for_This_patron to also work in admin view

--- app/models/valid_voucher.rb
+++ app/models/valid_voucher.rb
@@ -55,6 +55,7 @@ class ValidVoucher < ActiveRecord::Base
 
   def max_sales_for_this_patron
     @max_sales_for_this_patron ||= max_sales_for_type()
+    [@max_sales_for_this_patron, showdate.max_sales].min
   end
 
   def self.from_params(valid_vouchers_hash)
