2320d7cfcfab0df7d2195a024d6f731c1f69e880
Armando Fox <fox@cs.berkeley.edu>
2017-03-17 12:50:44 -0700

    only use mysql special initialization when mysql adapter active

--- config/initializers/abstract_mysql_adapter_primary_key.rb
+++
@@ -1,5 +0,0 @@
-# This can be removed for Rails >=3 rails3
-
-class ActiveRecord::ConnectionAdapters::MysqlAdapter  
-  NATIVE_DATABASE_TYPES[:primary_key] = "int(11) auto_increment PRIMARY KEY"
-end
