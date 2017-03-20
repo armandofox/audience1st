66737c100b11cafac1cf7c5b09009ba3d56011f5
Armando Fox <fox@cs.berkeley.edu>
2017-03-19 19:14:46 -0700

    fix javascript loading

---
+++ app/helpers/javascript_tag_helper.rb
@@ -0,0 +1,13 @@
+module JavascriptTagHelper
+  ## Load jQuery, rails.js, and everything in the Javascript dir.
+  ## Obsoleted by asset pipeline in Rails 3
+  
+  def javascript_include_tag_for_jquery(*args)
+    default_files = %w(jquery.min jquery-ui.min rails)
+    other_files = Dir["#{ActionView::Helpers::AssetTagHelper::JAVASCRIPTS_DIR}/*.js"].
+      map { |s| File.basename(s).gsub(/\.js$/,'') }.
+      delete_if { |s| default_files.include? s }
+    javascript_include_tag(default_files, :cache => true) << "\n" <<
+      javascript_include_tag(other_files, :cache => true)
+  end
+end
