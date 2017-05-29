bb9a57fb7af4b746159ed378af2ce669eb91f03f
Armando Fox <fox@cs.berkeley.edu>
2017-05-29 01:10:30 -0700

    The css class _autocomplete (for customer search) was automatically loading the customer index page for the autocompleted customer.  This was the wrong thing when autocmplete was being used to transfer a voucher or limit a donation search.  Now the autoload of the customer index page only happens if the autocomplete field ALSO has class _autosubmit.

--- public/javascripts/customer_search.js
+++ public/javascripts/customer_search.js
@@ -8,11 +8,15 @@ A1.select_search_result = function(customer,textField,idField) {
   //   with the chosen name.
   textField.val(customer.item.label);
   idField.val(customer.item.value);
-  window.location.assign($('#id').val());
+  // If the text field ALSO has the class '_autosubmit', visit the 
+  //   customer's page.
+  if (textField.hasClass('_autosubmit')) {  
+      window.location.assign($('#id').val());
+  }
 };
 
 A1.setup_autocomplete_fields = function() {
-  var autocomplete_url = jQuery('#autocomplete_route').val().toString();
+  var autocomplete_url = $('#autocomplete_route').val().toString();
   $(A1.autocomplete_selector).each(function(i,elt) {
     var e = $(elt);
     // which ID field is associated with this autocomplete element?
