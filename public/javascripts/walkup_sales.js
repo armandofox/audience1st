029536b5acc1213a74766eba2440ef69aeebbf38
Armando Fox <fox@cs.berkeley.edu>
2017-06-10 17:55:57 -0700

    various javascript bugs in walkup sales code and CC swipe code

--- public/javascripts/walkup_sales.js
+++ public/javascripts/walkup_sales.js
@@ -1,11 +1,16 @@
 // walkup sales - calculator
 
 A1.recalc_store_total = function() {
-  var total = A1.recalculate('#total', '.itemQty', 2);
+  var total = A1.recalculate('#total', '.itemQty', 2, 'price');
   $('#submit').prop('disabled', (total <= 0.0));
 };
 
-A1.recalculate = function(total_field,selector,decplaces) {
+A1.recalc_all_walkup_sales = function() {
+  A1.recalculate('#totaltix', '.itemCount', 0);
+  A1.recalculate('#total', '.item', 2, 'price');
+}
+
+A1.recalculate = function(total_field,selector,decplaces,attrib) {
   $(total_field).value = '';
   var tot = 0.0;
   var elts = $(selector);
@@ -13,15 +18,18 @@ A1.recalculate = function(total_field,selector,decplaces) {
   var qty;
   elts.each(function(i) {
     var elt = $(this);
-    if ((price = elt.data('price')) == undefined) { 
-      // if data-price is not set, the field value itself is the 'price'
+    if ((typeof attrib == "undefined") || 
+        ((price = elt.data(attrib)) == undefined)) { 
+      // if price attribute is either not given or not present,
+      // the field value itself is the 'price'
       price = 1.0;
     } 
     qty = parseFloat(elt.val());
-    if (isNaN(qty)) { qty = 0; }
+    if (isNaN(qty)) { 
+      qty = 0; 
+    }
     tot += (price * qty);
   });
-  if (!decplaces && decplaces != 0) { decplaces = 2; }
   $(total_field).val(tot.toFixed(decplaces));
   return(tot);
 }
@@ -29,5 +37,7 @@ A1.recalculate = function(total_field,selector,decplaces) {
 $(function() {
   $('#store_index .itemQty').change(A1.recalc_store_total);
   $('#store_subscribe .itemQty').change(A1.recalc_store_total);
+  // for walkup sales page
+  $('#walkup_tickets .item').change(A1.recalc_all_walkup_sales);
 });
 
