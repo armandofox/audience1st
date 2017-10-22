5f8933ce33187f49e6cb4253045fa5e01e7d99d6
Armando Fox <fox@cs.berkeley.edu>
2017-10-22 09:38:44 -0700

    lapsed_subscribers_spec was calling an expectation matcher in a way that worked in rspec 2 but not rspec 3; updated lapsed_subscribers_spec to use expect rather than should

--- spec/models/reports/lapsed_subscribers_spec.rb
+++ spec/models/reports/lapsed_subscribers_spec.rb
@@ -4,31 +4,31 @@ describe LapsedSubscribers do
   before :each do ;  @report = LapsedSubscribers.new ; end
   context 'when called with empty array(s)' do
     it 'should not raise error' do
-      lambda { @report.generate() }.should_not raise_error
+      expect { @report.generate() }.not_to raise_error
     end
     it 'should return nil' do
-      @report.generate.should be_nil
+      expect(@report.generate).to be_nil
     end
     it 'should include an error' do
       @report.generate
-      @report.errors.should include_match_for(/specify at least one/)
+      expect(@report.errors).to match /specify at least one/
     end
   end
   context 'when one array is empty' do
     it 'should not call purchased_any if have[] is empty' do
-      Customer.should_not_receive(:purchased_any_vouchertypes)
-      Customer.should_receive(:purchased_no_vouchertypes).with([1,2]).and_return([])
+      expect(Customer).not_to receive(:purchased_any_vouchertypes)
+      expect(Customer).to receive(:purchased_no_vouchertypes).with([1,2]).and_return([])
       @report.generate(:dont_have_vouchertypes => ['0,1,2'])
     end
     it 'should not call purchased_none if dont_have[] empty' do
-      Customer.should_receive(:purchased_any_vouchertypes).and_return([])
-      Customer.should_not_receive(:purchased_no_vouchertypes).with([1,2])
+      expect(Customer).to receive(:purchased_any_vouchertypes).and_return([])
+      expect(Customer).not_to receive(:purchased_no_vouchertypes).with([1,2])
       @report.generate(:have_vouchertypes => ['0,1,2'])
     end
     it 'should call both scopes if both arrays nonempty' do
-      Customer.should_receive(:purchased_any_vouchertypes).and_return([])
-      Customer.should_receive(:purchased_no_vouchertypes).and_return([])
-      @report.generate(:dont_have_vouchertypes => ['1,0'], :have_vouchertypes => ['0,1,2']).should be_empty
+      expect(Customer).to receive(:purchased_any_vouchertypes).and_return([])
+      expect(Customer).to receive(:purchased_no_vouchertypes).and_return([])
+      expect(@report.generate(:dont_have_vouchertypes => ['1,0'], :have_vouchertypes => ['0,1,2'])).to be_empty
     end
   end
 end
