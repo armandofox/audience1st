27c76d4ff5a03935e7e3248e93c4aaf004908bd1
sanketq <sanketq@gmail.com>
2017-11-06 16:40:11 -0800

    encrypt is still needed for old customers, re-enabled it

--- spec/models/customer_authentication_spec.rb
+++ spec/models/customer_authentication_spec.rb
@@ -42,10 +42,9 @@ describe Customer, 'authentication' do
 
   # bcrypted password
   it "authenticates a user against a hard-coded bcrypted password" do
-    password = 'pass'
     old_password_holder = create :customer, email: 'salty_dog@example.com', bcrypted_password: '$2a$10$neM5JoarHPcjHQz2xYmfp.5mkNj8euTrJSG9qkVvfBtwrrbPN5oMG', created_at: 1.day.ago
     c = Customer.authenticate(old_password_holder.email, 'test')
-    c.should == true
+    c.should == old_password_holder
     c.errors.should be_empty
   end
 
