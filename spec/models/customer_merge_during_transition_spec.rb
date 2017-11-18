3ac168ae147d793d0bfe12dd4ae4065ea9764eaf
WanNJ <omyjackwan@gmail.com>
2017-11-18 00:08:15 -0800

    [Merge] Finish this story, but leave some issues to be solved during next meeting.
    When merge a new user into a old user, I tried to get rid of the email and the password & salt in the customer table but get this:
    
    ActiveModel::MassAssignmentSecurity::Error Exception: Can't mass-assign protected attributes for Customer: crypted_password, salt.
    
    For now, we can:
    * Change it -> So that we can know which customer has been migrated.
    * Keep it -> Harder to determine the boundary but more safe.

--- spec/models/customer_merge_during_transition_spec.rb
+++ spec/models/customer_merge_during_transition_spec.rb
@@ -13,44 +13,56 @@ describe Customer, "merging during transition period" do
       @user_keep.save!
       @user_merged.save!
       allow(@user_merged).to receive(:fresher_than?).and_return(true)
-      allow(Authorization).to receive(:find_by_provider_and_uid).with('Identity', @user_keep.email).and_return(nil)
-      allow(Authorization).to receive(:find_by_provider_and_uid).with('Identity', @user_merged.email).and_return(nil)
       allow(Customer).to receive(:update_foreign_keys_from_to).and_return(['Wat, Wat'])
     end
     it "should use the old way to store the password" do
       merge(@user_keep, @user_merged)
-      expect(Authorization.where(:uid => @user_merged.email).length).to eq 0
+      expect(Authorization.find_by_provider_and_customer_id('Identity', @user_keep.id)).to be_nil
     end
 
     it "should change the older user's password to the fresher user's password" do
       merge(@user_keep, @user_merged)
-      expect(Customer.where(:email => @user_merged.email).first.crypted_password).to eq @user_merged.crypted_password
+      expect(Customer.find(@user_keep.id).crypted_password).to eq @user_merged.crypted_password
     end
   end
 
-  # describe "merge new user to old users" do
-  #   before(:each) do
-  #     @user_keep = create(:old_customer)
-  #     @user_merged = create(:customer)
-  #     allow(@user_merged).to receive(:fresher_than?).and_return(true)
-  #   end
-  #   it "should create a anthentification through the new way" do
-  #     pending
-  #   end
-  #
-  #   it "should delete the email and password of the old user from the customers table" do
-  #     pending
-  #   end
-  # end
-  #
-  # describe "merge old user to new user" do
-  #   before(:each) do
-  #     @user_keep = create(:customer)
-  #     @user_merged = create(:old_customer)
-  #     allow(@user_merged).to receive(:fresher_than?).and_return(true)
-  #   end
-  #   it "should keep the current password" do
-  #     pending
-  #   end
-  # end
+  describe "merge new user to old users" do
+    before(:each) do
+      @user_keep = create(:old_customer)
+      @user_merged = create(:customer)
+      @user_keep.save!
+      @user_merged.save!
+      allow(@user_merged).to receive(:fresher_than?).and_return(true)
+      allow(Customer).to receive(:update_foreign_keys_from_to).and_return(['Wat, Wat'])
+    end
+    it "should create a anthentification through the new way" do
+      pass = Authorization.find_by_provider_and_customer_id('Identity', @user_merged.id).password_digest
+      merge(@user_keep, @user_merged)
+      expect(Authorization.find_by_provider_and_customer_id('Identity', @user_keep.id).password_digest).to eq(pass)
+    end
+
+    # For the following test, it's a matter of migration strategy, so just leave it alone for now.
+
+    # it "should keep the email and password of the old user from the customers table" do
+    #   merge(@user_keep, @user_merged)
+    #   expect(Customer.find(@user_keep.id).email).must_be_empty
+    #   expect(Customer.find(@user_keep.id).crypted_password).to must_be_empty
+    # end
+  end
+
+  describe "merge old user to new user" do
+    before(:each) do
+      @user_keep = create(:customer)
+      @user_merged = create(:old_customer)
+      @user_keep.save!
+      @user_merged.save!
+      allow(@user_merged).to receive(:fresher_than?).and_return(true)
+      allow(Customer).to receive(:update_foreign_keys_from_to).and_return(['Wat, Wat'])
+    end
+    it "should keep the current password" do
+      pass = Authorization.find_by_provider_and_customer_id('Identity', @user_keep.id).password_digest
+      merge(@user_keep, @user_merged)
+      expect(Authorization.find_by_provider_and_customer_id('Identity', @user_keep.id).password_digest).to eq(pass)
+    end
+  end
 end
