f1dfffa9c95946fe20a9ba5969c9e49c280b2092
andrewsun98 <potatosole@gmail.com>
2017-12-02 18:38:00 -0800

    Edited form after change to comments from best way to contact

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,6 +1,6 @@
 class Group < ActiveRecord::Base
 
-  attr_accessible :name, :address_line_1, :address_line_2, :city, :state, :zip, :work_phone, :cell_phone, :work_fax, :group_url, :best_way_to_contact, :type
+  attr_accessible :name, :address_line_1, :address_line_2, :city, :state, :zip, :work_phone, :cell_phone, :work_fax, :group_url, :comments, :type
   has_and_belongs_to_many :customers
   self.inheritance_column = :type
 
