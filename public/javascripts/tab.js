b2778256796a33992dd5e6b9e6deca5db9ee1192
Zhiying Liang <wayneleung@berkeley.edu>
2017-12-03 00:10:06 -0800

    fix a weird bug about js

--- public/javascripts/tab.js
+++ public/javascripts/tab.js
@@ -1,18 +1,20 @@
 window.onload = function(){
     var myTab = document.getElementById("tab");
-    var myUl = myTab.getElementsByTagName("ul")[0];
-    var myLi = myUl.getElementsByTagName("li");
-    var myDiv = myTab.getElementsByTagName("div");
+    if (myTab != null) {
+        var myUl = myTab.getElementsByTagName("ul")[0];
+        var myLi = myUl.getElementsByTagName("li");
+        var myDiv = myTab.getElementsByTagName("div");
     
-    for(var i = 0; i<myLi.length;i++){
-        myLi[i].index = i;
-        myLi[i].onclick = function(){
-            for(var j = 0; j < myLi.length; j++){
-                myLi[j].className="off";
-                myDiv[j].className = "hide";
+        for(var i = 0; i<myLi.length;i++){
+            myLi[i].index = i;
+            myLi[i].onclick = function(){
+                for(var j = 0; j < myLi.length; j++){
+                    myLi[j].className="off";
+                    myDiv[j].className = "hide";
+                }
+                this.className = "on";
+                myDiv[this.index].className = "show";
             }
-            this.className = "on";
-            myDiv[this.index].className = "show";
         }
-      }
     }
+}
