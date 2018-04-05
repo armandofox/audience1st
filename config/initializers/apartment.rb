a796b3b40298548dd13cedd549cf5f49fec3a50b
Armando Fox <fox@cs.berkeley.edu>
2018-04-04 18:08:18 -0700

    need to use first subdomain for heroku deploy

--- config/initializers/apartment.rb
+++ config/initializers/apartment.rb
@@ -4,8 +4,8 @@
 #
 # require 'apartment/elevators/generic'
 # require 'apartment/elevators/domain'
-require 'apartment/elevators/subdomain'
-# require 'apartment/elevators/first_subdomain'
+# require 'apartment/elevators/subdomain'
+require 'apartment/elevators/first_subdomain'
 # require 'apartment/elevators/host'
 
 #
@@ -96,8 +96,8 @@ end
 # }
 
 # Rails.application.config.middleware.use Apartment::Elevators::Domain
-Rails.application.config.middleware.use Apartment::Elevators::Subdomain
-# Rails.application.config.middleware.use Apartment::Elevators::FirstSubdomain
+# Rails.application.config.middleware.use Apartment::Elevators::Subdomain
+Rails.application.config.middleware.use Apartment::Elevators::FirstSubdomain
 # Rails.application.config.middleware.use Apartment::Elevators::Host
 # Rails.application.config.middleware.use Apartment::Elevators::Generic, lambda { |req|
 #   req.path.split('/')[1]
