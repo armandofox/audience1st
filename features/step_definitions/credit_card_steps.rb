cd516dc91d459e4424f4e0393c5b057682e00120
Armando Fox <fox@cs.berkeley.edu>
2018-04-13 14:48:08 -0700

    got stripe force-failure to work, but it seems weird to have to bypass js library checks

--- features/step_definitions/credit_card_steps.rb
+++ features/step_definitions/credit_card_steps.rb
@@ -3,10 +3,8 @@ When /^I fill in an? (in)?valid credit card for "(.*) (.*)"$/ do |valid,first,la
     fill_in "First Name", :with => first
     fill_in "Last Name", :with => last
     fill_in "credit_card_month", :with => Time.now.month
-    fill_in "credit_card_year", :with => '2050'
+    fill_in "credit_card_year", :with => 1 + Time.now.year
     fill_in "Security (CVV) code", :with => '333'
-    fill_in "Number (no spaces)", :with =>
-      valid =~ /in/ ? '4000000000000002' : '4242424242424242'
-    # invalid card number is Stripe's test number for "card declined"
+    fill_in "Number (no spaces)", :with => '4242424242424242'
   end
 end
