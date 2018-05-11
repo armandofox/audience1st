f5a39d47a91dda9c9990ef2aee2518d29c37ec40
Armando Fox <fox@cs.berkeley.edu>
2018-05-11 16:28:16 -0700

    get rid of bogus :escape =>false in store URLs

--- app/helpers/shows_helper.rb
+++ app/helpers/shows_helper.rb
@@ -20,14 +20,12 @@ module ShowsHelper
   def link_to_show_tickets(show)
     params = {:show_id => show.id}
     params[:what] = 'Special Events' if show.special?
-    params[:escape] = false
-    store_url(params)
+    store_url(params).html_safe
   end
 
   def link_to_showdate_tickets(showdate, params={})
     params[:showdate_id] = showdate.id
     params[:what] = 'Special Events' if showdate.show.special?
-    params[:escape] = false
     store_url(params).html_safe
   end
 
