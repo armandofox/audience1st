e8f72c93514a9db0cacf3913f3e98a6fa9c809a7
Armando Fox <fox@cs.berkeley.edu>
2018-07-27 10:48:21 +0200

    reenable ssl in production! doh!

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
 
   protect_from_forgery
 
-  #force_ssl if Rails.env.production?
+  force_ssl if Rails.env.production?
   
   include AuthenticatedSystem
   include FilenameUtils
