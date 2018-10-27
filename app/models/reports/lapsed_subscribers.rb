fe5f0331418540eba26b2aee377f13ce876aec44
Armando Fox <fox@cs.berkeley.edu>
2018-10-27 13:57:02 +0200

    lapsed_subcsribers still wasnt using a Relation at the end

--- app/models/reports/lapsed_subscribers.rb
+++ app/models/reports/lapsed_subscribers.rb
@@ -16,7 +16,7 @@ class LapsedSubscribers < Report
     add_error("You must specify at least one type of voucher from at least one list.") and return if (have.empty? && have_not.empty?)
     @relation = have.empty? ? Customer.regular_customers : Customer.regular_customers.purchased_any_vouchertypes(have)
     if ! (have_not.empty?)
-      @relation = @relation & Customer.purchased_no_vouchertypes(have_not)
+      @relation = @relation.merge(Customer.purchased_no_vouchertypes(have_not))
     end
     @relation
   end
