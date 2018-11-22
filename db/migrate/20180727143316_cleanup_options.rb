d8362babc6eb0a0a7418211c3ee0d0e3286f2ad6
Armando Fox <fox@cs.berkeley.edu>
2018-11-22 13:10:24 +0100

    rebuild schema.rb and delete old migrations

--- db/migrate/20180727143316_cleanup_options.rb
+++
@@ -1,8 +0,0 @@
-class CleanupOptions < ActiveRecord::Migration
-  def change
-    # remove unused venue_id and venue_shortname fields
-    remove_column :options, :venue_id
-    remove_column :options, :venue_shortname
-    remove_column :options, :sendgrid_key_name
-  end
-end
--- db/migrate/20180920121517_add_option_to_allow_guest_checkout.rb
+++
@@ -1,5 +0,0 @@
-class AddOptionToAllowGuestCheckout < ActiveRecord::Migration
-  def change
-    add_column :options, :allow_guest_checkout, :boolean, :default => false
-  end
-end
