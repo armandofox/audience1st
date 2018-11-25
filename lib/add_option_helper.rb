33675762450f92869b11a993183b7b244b7082c2
Armando Fox <fox@cs.berkeley.edu>
2018-11-25 19:41:51 +0100

    remove vestigial file

--- lib/add_option_helper.rb
+++
@@ -1,9 +0,0 @@
-class AddOptionHelper
-
-  def self.add_new_option(id,grp,name,value,typ)
-    newopt = Option.create!(:grp => grp, :name => name, :value => value.to_s,
-                           :typ => typ)
-    ActiveRecord::Base.connection.execute("UPDATE options SET id=#{id} WHERE id=#{newopt.id}")
-  end
-
-end
