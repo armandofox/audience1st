dd9acdb7262239f76685a7c58db8c02ded143e6c
Armando Fox <fox@cs.berkeley.edu>
2018-11-29 13:02:50 +0100

    fix credit card steps for new markup

--- features/step_definitions/credit_card_steps.rb
+++ features/step_definitions/credit_card_steps.rb
@@ -1,9 +1,9 @@
-When /^I fill in an? (in)?valid credit card for "(.*)"$/ do |valid,name|
+When /^I fill in an? (in)?valid credit card for "(.*)"$/ do |invalid,name|
   within "#credit_card" do
     fill_in "Name", :with => name
-    fill_in "credit_card_month", :with => Time.current.month
-    fill_in "credit_card_year", :with => 1 + Time.current.year
+    select Time.current.month, :from => 'credit_card_month'
+    select 1+Time.current.year, :from => 'credit_card_year'
     fill_in "CVV code", :with => '333'
-    fill_in "Number", :with => '4242424242424242'
+    fill_in "Number", :with => (invalid =~ /in/ ? '9999999999999999' : '4242424242424242')
   end
 end
