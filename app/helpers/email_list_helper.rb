25fc733a00ffbe29689ab99f28d1c576630bdffb
Armando Fox <fox@cs.berkeley.edu>
2018-12-23 19:52:26 -0800

    fix rspec test typos; still need to verify logic for email sublist stuff

--- app/helpers/email_list_helper.rb
+++
@@ -1,9 +0,0 @@
-module EmailListHelper
-
-  def options_for_sublists(sublists=[])
-    options_for_select(sublists.map do |name,count|
-        [h("#{name} (#{count} members)"), h(name)]
-      end)
-  end
-
-end
