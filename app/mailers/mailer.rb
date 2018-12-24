b99deefc1151dc500918b5eff3d4b5ea2ce7747e
Armando Fox <fox@cs.berkeley.edu>
2018-12-23 17:11:55 -0800

    try to turn off email in staging

--- app/mailers/mailer.rb
+++ app/mailers/mailer.rb
@@ -67,13 +67,11 @@ class Mailer < ActionMailer::Base
   def setup_delivery_params
     if (Option.sendgrid_key_value.blank? or
         Option.sendgrid_domain.blank?)
-      Rails.application.config.action_mailer.perform_deliveries = false
-      Rails.application.config.action_mailer.delivery_method = :test
+      ActionMailer::Base.perform_deliveries = false
       Rails.logger.info "NOT sending email"
     else
       Rails.logger.info "Setting up sendgrid"
-      Rails.application.config.action_mailer.perform_deliveries = true
-      Rails.application.config.action_mailer.delivery_method = :smtp
+      ActionMailer::Base.perform_deliveries = true
       ActionMailer::Base.smtp_settings = {
         :user_name => 'apikey',
         :password => Option.sendgrid_key_value,
--- config/environments/migration.rb
+++
@@ -1,2 +0,0 @@
-# This empty file is necessary for RAILS_ENV=migration to be a valid
-# environment, used only for running database-schema-altering operations.
