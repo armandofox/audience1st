1f12b71baeefea448acaed84bc3d26aa5d5b11b8
Kevin <crazyberry7@gmail.com>
2019-05-02 20:07:56 -0700

    Added Today Tix CSV validity checks

--- app/services/ticket_sales_import_parser/today_tix.rb
+++ app/services/ticket_sales_import_parser/today_tix.rb
@@ -1,6 +1,6 @@
 module TicketSalesImportParser
   class TodayTix
-
+    require 'csv'
     attr_reader :import
     delegate :raw_data, :to => :import
     
@@ -10,6 +10,29 @@ module TicketSalesImportParser
 
     # sanity-check that the raw data appears to be a valid import file
     def valid?
+      #byebug 
+      
+      # Check if CSV file is empty
+      if @import.raw_data.blank?
+        @import.errors.add(:vendor, "Data is invalid because file is empty")
+        false
+      end
+        
+      begin
+        csv_arr = CSV.parse(raw_data)
+      rescue Exception => e
+        @import.errors.add(:vendor, e)
+        false
+      end
+    
+      # Check if csv file contains nil or multiple words per entry
+
+      csv_arr.each do |row|
+          return false if row.include?(nil)
+          row.each do |entry|
+              return false if entry.split(' ').length > 1
+          end
+      end
       true
       # if errors are found, modify the errors objecton the import itself:
       # @import.errors.add(:vendor, "Data is invalid because " + explanation_of_whats_wrong)
