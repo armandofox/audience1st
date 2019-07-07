a50a56fb155b4af889e57700da199f625ed0eca6
Armando Fox <fox@cs.berkeley.edu>
2019-07-07 09:37:55 -0700

    mark CustomerIMport tests pending since we will rewrite or eliminate that logic; fix Capybara warnings about menu selectors not being strings

--- features/support/wait_for_ajax.rb
+++ features/support/wait_for_ajax.rb
@@ -3,6 +3,7 @@ module WaitForAjax
     Capybara.current_driver == Capybara.javascript_driver
   end
   def wait_for_ajax
+    return
     return unless javascript_test?
     Timeout.timeout(Capybara.default_max_wait_time) do
       1 until finished_all_ajax_requests?
