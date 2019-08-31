0d2d3289aab4a3c00a2f388a0cf0b6eed1e2a0fe
Armando Fox <fox@cs.berkeley.edu>
2019-08-31 11:47:57 -0700

    thwarted attempt to display wheelchair icon as seat label

--- app/models/seatmap.rb
+++ app/models/seatmap.rb
@@ -1,7 +1,9 @@
 class Seatmap < ActiveRecord::Base
 
   require 'csv'
-  
+
+  VALID_SEAT_LABEL_REGEX = /^\s*[A-Za-z0-9]+\+?\s*$/
+
   validates :name, :presence => true, :uniqueness => true
   validates :csv, :presence => true
   validates :json,  :presence => true
@@ -22,6 +24,11 @@ class Seatmap < ActiveRecord::Base
     seat_list.match Regexp.new("\\b#{seat}\\b")
   end
 
+  def update!
+    parse_csv
+    save!
+  end
+
   def parse_csv
     @as_js = []
     list = []
@@ -30,12 +37,19 @@ class Seatmap < ActiveRecord::Base
     @rows.each do |row|
       row_string = ''
       row.each do |cell|
-        cell = cell.strip.upcase
         if cell.blank?   # no seat in this location
           row_string << '_'
         else
+          cell = cell.strip.upcase
+          return errors.add(:base, "Invalid seat label #{cell} (may contain only letters, numbers, and trailing + sign for accessible seats") unless cell =~ VALID_SEAT_LABEL_REGEX
           seat_type = (cell.sub!( /\+$/, '') ? 'a' : 'r') # accessible or regular seat
-          row_string << "#{seat_type}[#{cell}, ]"
+          # icons don't work with jQuery seatmaps yet...
+          # label = (seat_type == 'r' ? ' ' : '\u267F')     # unicode HTML glyph for wheelchair
+          # and the 'A' labels for accessible seats don't align right...
+          # label = (seat_type == 'r' ? ' ' : 'A')
+          # so just fall back for now
+          label = ' '
+          row_string << "#{seat_type}[#{cell},#{label}]"
           list << cell
         end
       end
