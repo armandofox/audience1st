92953b824e85e521b020837d2235a562455556f1
Armando Fox <fox@cs.berkeley.edu>
2019-08-31 15:42:47 -0700

    Cancel/Start Over at checkout or shipping info now clears out and destroys the stale order

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -61,6 +61,7 @@ class ApplicationController < ActionController::Base
 
   def reset_shopping           # called as a filter
     session.delete(:promo_code)
+    @gOrderInProgress.destroy if @gOrderInProgress
     set_order_in_progress(nil)
     session.delete(:return_to)
     true
