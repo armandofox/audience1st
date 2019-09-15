224452be96a81c3698efebb0d42ddf9b15454847
Armando Fox <fox@cs.berkeley.edu>
2019-09-15 12:53:16 -0700

    Finishes 168229623. New checkout.js assumes all JS responsibilities for in-progress orders (so "checkout message" logic got moved out of checkout.html.haml). New styles for both the div containing the countdown timer and the numbers of the timer itself. order_timeout (in minutes) added to Options, with allowed range 1 to 15 minutes, default 5.

---
+++ app/assets/javascripts/checkout.js
@@ -0,0 +1,49 @@
+A1.checkout = {
+  showCheckoutWarning: function() {
+    var message = $('#checkout_message').val();
+    if (message != '') {
+      alert(message);
+    }
+  }
+  ,startTimer: function() {
+    var timerExpiresAt = Number($('#timer_expires').val()); // seconds since epoch, from Ruby
+    var now = (Date.now() / 1000) >> 0; // seconds since epoch; Date.now returns millisecs
+    var diff = timerExpiresAt - now;
+    var handler;
+    if (diff > 0) {
+      $('.timer').removeClass('d-none');
+      handler = setInterval(updateTimer, 1000);
+    } else {
+      A1.checkout.timerExpired();
+    };
+    function updateTimer() {
+      var minutes = (diff / 60) >> 0; // coerce to integer
+      var seconds = diff % 60;
+      var timerString = minutes + ':' + (seconds < 10 ? '0' : '') + seconds;
+      $('#timer').html(timerString);
+      diff -= 1;
+      if (diff < 0) {
+        diff = 0;               // we're done
+        clearInterval(handler);
+        A1.checkout.timerExpired();
+      };
+    };
+  }
+  ,timerExpired: function() {
+    // show alert and cancel the order
+    alert("Sorry, but you did not complete the order in time.  Your seats have been released.  Please restart your order.");
+    window.location.replace('/store/cancel'); // replace makes current page inaccessible in History
+  }
+  ,setupForCheckout: function() {
+    if ($('#cart').length > 0) {
+      // on any page where in-progress order is displayed, show countdown timer
+      A1.checkout.startTimer();
+    }
+    if ($('body#store_checkout').length > 0) {
+      // on checkout page only, show checkout message
+      A1.checkout.showCheckoutWarning();
+    };
+  }
+};
+
+$(A1.checkout.setupForCheckout);
