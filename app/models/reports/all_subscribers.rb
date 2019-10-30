80a0fd0d328cb40b64636bfcb6ecac9e44062fd4
chengchenghuang <cicihuang@berkeley.edu>
2019-10-29 20:52:41 -0700

    syntax correct

--- app/models/reports/all_subscribers.rb
+++ app/models/reports/all_subscribers.rb
@@ -3,20 +3,19 @@ class AllSubscribers < Report
   def initialize(output_options = {})
     @view_params = {
       :name => 'All subscribers',
-      :default_voucher_type => "???"
+      :vouchertypes => Vouchertype.subscription_vouchertypes()
     }
     super
   end
 
   def generate(params = {})
-    vouchertypes = params[:vouchertypes]
+    vouchertypes = Report.list_of_ints_from_multiselect(params[:vouchertypes]) # From app/models/reports/subscriber_open_vouchers.rb
     puts vouchertypes
     @relation = 
-      if vouchertypes.empty? # TODO: add an "all_sub" option
+      if vouchertypes.empty? # TODO: add an "all_sub" option, returns all subscription_vouchertypes
       then Customer.purchased_any_vouchertypes(Vouchertype.subscription_vouchertypes.map(&:id))
       else 
         #TODO: make use of list_of_ints_from_multiselect(), and modify app/views/reports/special/_all_subscribers.html.haml to match the list_of_ints_from_multiselect's requirement 
-        vouchertypes = Report.list_of_ints_from_multiselect(params[:vouchertypes]) # From app/models/reports/subscriber_open_vouchers.rb
         Customer.purchased_any_vouchertypes(vouchertypes) #TODO: handle multiple selections
       end
   end
