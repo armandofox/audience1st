984a883da5b9ee5af6da0d74ece24b09ec6553c4
Xu Huang <hexhuang@berkeley.edu>
2019-11-02 13:25:19 -0700

    In Customer Lists - All subscribers, filter by VoucherTypes categories or the seasons

--- app/models/reports/all_subscribers.rb
+++ app/models/reports/all_subscribers.rb
@@ -9,15 +9,21 @@ class AllSubscribers < Report
   end
 
   def generate(params = {})
+    #TODO: affirm filter logic. Make sure generated query matches selections.
+    #TODO: maybe we can use app/views/reports/index.html.haml#line 33~38 as a reference?
     vouchertypes = Report.list_of_ints_from_multiselect(params[:vouchertypes]) # From app/models/reports/subscriber_open_vouchers.rb
+    seasons = params[:seasons]
     puts vouchertypes
     @relation = 
       if vouchertypes.empty? # TODO: add an "all_sub" option, returns all subscription_vouchertypes
       then Customer.purchased_any_vouchertypes(Vouchertype.subscription_vouchertypes.map(&:id))
-      else 
-        #TODO: make use of list_of_ints_from_multiselect(), and modify app/views/reports/special/_all_subscribers.html.haml to match the list_of_ints_from_multiselect's requirement 
-        Customer.purchased_any_vouchertypes(vouchertypes) #TODO: handle multiple selections
+      else Customer.purchased_any_vouchertypes(vouchertypes)
       end
+    @relation = @relation.subscriber_during seasons unless seasons.empty?
+    #TODO: currently the generated resport only contains 26 consumers 
+    # when using fake data, whihch is less than the "Subscription Counts" (Qty = 62).
+    # Is this correct?
+    #TODO: test the above query
   end
 end
 
