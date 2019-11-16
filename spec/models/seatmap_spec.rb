0df55d8cedac88414c8ed54d236209291179cc72
chengchenghuang <cicihuang@berkeley.edu>
2019-11-16 13:46:53 -0800

    done with deleting order comment from oders view

--- spec/models/seatmap_spec.rb
+++ spec/models/seatmap_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-describe Seatmap, focus: true do
+describe Seatmap do
   describe 'JSON' do
     before(:each) do
       @sd = create(:showdate, :seatmap => (@s = create(:seatmap)))
