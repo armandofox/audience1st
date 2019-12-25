a20edf59c2b8f84be2498132ea92ebac0cbb9bb6
Armando Fox <fox@cs.berkeley.edu>
2019-12-24 18:29:47 -0800

    forgo to add stale order sweeper class

---
+++ app/services/stale_order_sweeper.rb
@@ -0,0 +1,15 @@
+class StaleOrderSweeper
+  # Deletes stale orders (abandoned and timeout has elapsed)
+  # Because all of an order's Items are destroyed along with it, we only need to
+  # operate on Orders, and ActiveRecord callbacks will take care of the rest.
+  # There's only one class method, designed to be called from a periodic runner or similar.
+
+  def self.sweep!
+    # destroy stale orders and update sweep timer
+    stale_date = (Option.order_timeout + 1).minutes.ago # just to be on the safe side
+    Order.transaction do
+      Order.abandoned_since(stale_date).destroy_all
+      Option.first.update_attributes!(:last_sweep => Time.current)
+    end
+  end
+end
