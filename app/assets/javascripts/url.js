b3ca5bca4529ea034d8756ec076a60d01e9c5a59
Armando Fox <fox@cs.berkeley.edu>
2020-01-24 18:21:45 -0800

    fixes 170118562

---
+++ app/assets/javascripts/url.js
@@ -0,0 +1,12 @@
+A1.replaceUrlParam = function(url, paramName, paramValue)
+{
+    if (paramValue == null) {
+        paramValue = '';
+    }
+    var pattern = new RegExp('\\b('+paramName+'=).*?(&|#|$)');
+    if (url.search(pattern)>=0) {
+        return url.replace(pattern,'$1' + paramValue + '$2');
+    }
+    url = url.replace(/[?#]$/,'');
+    return url + (url.indexOf('?')>0 ? '&' : '?') + paramName + '=' + paramValue;
+}
