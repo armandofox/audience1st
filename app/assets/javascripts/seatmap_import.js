f9cb3c5140945125a02aac462a1fbc8162ab1881
Armando Fox <fox@cs.berkeley.edu>
2020-02-02 19:49:05 -0800

    Confirm logic now needs to record the seat assignments, and re-eanble other Choose Seats buttons; Choose Seats logic needs to start by unregistering any previously chosen seats

--- app/assets/javascripts/seatmap_import.js
+++ app/assets/javascripts/seatmap_import.js
@@ -1,9 +1,11 @@
 A1.ticketSalesImport = {
   showMap: function(evt) {
-    var container = $(this).closest('.import-row');
+    var choose = $(this);
+    var container = choose.closest('.import-row');
     var showdateID = container.find('.showdate-id').val();
     var selectedSeats = container.find('.seat-display');
     var chooseSeats = $('.select-seats');
+    var confirm = container.find('.confirm-seats');
 
     evt.preventDefault();
     // move the hidden table row to just below our own, and reveal it
@@ -16,25 +18,33 @@ A1.ticketSalesImport = {
     // once seat selection begins, must choose all seats for this order, OR cancel,
     // before can choose seats for another order
     chooseSeats.prop('disabled', true);
+    choose.addClass('d-none'); 
+    confirm.removeClass('d-none');
+    confirm.click(A1.ticketSalesImport.confirmSeats);
 
     A1.seatmap.max = Number(container.find('.num-seats').val());
     A1.seatmap.resetAfterCancel = function() {
       $('.tbody-import').append($('#seatmap-table-row'));
-      container.find('.display-seats').val('');
+      selectedSeats.val('');
       chooseSeats.prop('disabled', false);
+      confirm.addClass('d-none');
+      $(this).removeClass('d-none');
     };
     A1.seatmap.onSelect = function() {
       selectedSeats.val(A1.seatmap.selectedSeatsAsString);
       chooseSeats.prop('disabled', true);
     };
     A1.seatmap.allSeatsSelected = function() {
-      chooseSeats.prop('disabled', false);
+      confirm.prop('disabled', false);
     };
     $.getJSON('/ajax/seatmap/' + showdateID, function(json_data) {
       A1.seatmap.configureFrom(json_data);
       A1.seatmap.seats = $('#seatmap').seatCharts(A1.seatmap.settings);
       A1.seatmap.setupMap();
     });
+  }
+  ,confirmSeats: function() {
+    alert("Woohoo!");
   }
   ,setup: function() {
     if ($('body#ticket_sales_imports_edit').length > 0) {
