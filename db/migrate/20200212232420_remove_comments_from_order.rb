7f328f76609ba98e6cf6a83833674339a563f1d4
Armando Fox <fox@cs.berkeley.edu>
2020-02-13 09:51:51 -0800

    truncate comments that are too long

--- db/migrate/20200212232420_remove_comments_from_order.rb
+++ db/migrate/20200212232420_remove_comments_from_order.rb
@@ -3,20 +3,24 @@ class RemoveCommentsFromOrder < ActiveRecord::Migration
     orders = Order.where.not(:comments => [nil, '']).includes(:items)
     blank_comments = 0
     nonblank_comments = 0
+    invalid = []
     Order.transaction do
       orders.each do |o|
         o.items.each do |item|
           if item.comments.blank?
-            item.comments = o.comments
+            item.comments = o.comments.split(/\s*;\s*/).uniq.join('; ').truncate(255)
             blank_comments += 1
           else
-            item.comments = "#{item.comments.to_s}; #{o.comments}"
+            item.comments = "#{item.comments.to_s}; #{o.comments}".split(/\s*;\s*/).uniq.join('; ').truncate(255)
             nonblank_comments += 1
           end
+          invalid.push(item.id) unless item.valid?
           item.save(:validate => false)
         end
       end
       puts "Updated #{blank_comments} blank and #{nonblank_comments} nonblank comments on #{orders.size} orders"
+      puts "#{invalid.length} invalid items:"
+      puts invalid.join(',')
       remove_column :orders, :comments
     end
   end
