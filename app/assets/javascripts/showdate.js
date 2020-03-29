198b8746dd127f8ac78156bb935806ab8721b040
Armando Fox <fox@cs.berkeley.edu>
2020-03-29 15:55:54 -0700

    when menu changes from RS to GA, house cap field is cleared

--- app/assets/javascripts/showdate.js
+++ app/assets/javascripts/showdate.js
@@ -1,16 +1,17 @@
-
 A1.adjustHouseCap = function() {
   var chosenSeatmap = $(this);
-  if (chosenSeatmap.val() == '')    {  // general admission
-    $('.showdate-house-capacity').removeClass('.a1-passive-text-input').prop('readonly',false);
+  if (chosenSeatmap.val() == ''  &&  !A1.firstTrigger)    {  // general admission
+    $('.showdate-house-capacity').val('').removeClass('.a1-passive-text-input').prop('readonly',false);
   } else {
     var capacity = chosenSeatmap.find('option:selected').text().match( /\(([0-9]+)\)$/ )[1];
     $('.showdate-house-capacity').val(capacity).addClass('.a1-passive-text-input').prop('readonly',true);
   }
+  A1.firstTrigger = false;
 };
 
 // On pages that have a .showdate-house-capacity auto-updatable field, .showdate-seating-choices 
 // will be the menu that triggers it
 $(function() {
+  A1.firstTrigger = true;
   $('.showdate-seating-choices').change(A1.adjustHouseCap).trigger('change');
 });
