fa466a4d65e3b0498b22b75041b17a3618bb07c1
Armando Fox <fox@cs.berkeley.edu>
2020-05-05 14:06:15 -0700

    fix Makefile, restore previous migration to its original form in prep for produciton

--- db/migrate/20200412180412_add_streamable_fields_to_showdate.rb
+++ db/migrate/20200412180412_add_streamable_fields_to_showdate.rb
@@ -1,8 +1,8 @@
 class AddStreamableFieldsToShowdate < ActiveRecord::Migration
   def change
     change_table 'showdates' do |t|
-      t.boolean 'live_stream', :null => false, :default => false
-      t.boolean 'stream_anytime', :null => false, :default => false
+      t.boolean 'live_stream', :null => true, :default => nil
+      t.boolean 'stream_anytime', :null => true, :default => nil
       t.text 'access_instructions', :null => true
     end
   end
