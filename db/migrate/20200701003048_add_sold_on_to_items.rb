8a4dec0cd3ccef7d26da330e867a4534fedd90ef
Armando Fox <fox@cs.berkeley.edu>
2020-07-02 11:31:49 -0700

    don't wrap migration in txn

--- db/migrate/20200701003048_add_sold_on_to_items.rb
+++ db/migrate/20200701003048_add_sold_on_to_items.rb
@@ -4,11 +4,11 @@ class AddSoldOnToItems < ActiveRecord::Migration
     Item.reset_column_information
     # Set the sold_on date for CanceledItems to the date of cancellation (= item.updated_at)
     # Set the sold_on date for all other item types to the order's sold_on
-    Item.transaction do
-      Item.finalized.includes(:order).each do |i|
-        i.update_attribute(:sold_on,
-          (if i.kind_of?(CanceledItem) then i.updated_at else i.order.sold_on end))
-      end
+    # Item.transaction do  # not enough dyno memory to allow luxury of this safeguard...
+    Item.finalized.includes(:order).each do |i|
+      i.update_attribute(:sold_on,
+        (if i.kind_of?(CanceledItem) then i.updated_at else i.order.sold_on end))
+      # end
     end
   end
 end
