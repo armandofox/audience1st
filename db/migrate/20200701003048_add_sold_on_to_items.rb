64e029810c8b46aa7362bd304e34d8a5c4415490
Armando Fox <fox@cs.berkeley.edu>
2020-07-02 11:44:02 -0700

    strip-mine loop in migration

--- db/migrate/20200701003048_add_sold_on_to_items.rb
+++ db/migrate/20200701003048_add_sold_on_to_items.rb
@@ -5,10 +5,16 @@ class AddSoldOnToItems < ActiveRecord::Migration
     # Set the sold_on date for CanceledItems to the date of cancellation (= item.updated_at)
     # Set the sold_on date for all other item types to the order's sold_on
     # Item.transaction do  # not enough dyno memory to allow luxury of this safeguard...
-    Item.finalized.includes(:order).each do |i|
-      i.update_attribute(:sold_on,
-        (if i.kind_of?(CanceledItem) then i.updated_at else i.order.sold_on end))
-      # end
+    offset = 0
+    while (1)
+      items =  Item.finalized.offset(offset).limit(1000)
+      offset += 1000
+      break if items.empty?
+      items.each do |i|
+        i.update_attribute(:sold_on,
+          (if i.kind_of?(CanceledItem) then i.updated_at else i.order.sold_on end))
+      end
+      print "."
     end
   end
 end
