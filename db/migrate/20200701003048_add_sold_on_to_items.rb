49ac1a0c766b08333f526cc84813d25b2bc598ea
Armando Fox <fox@cs.berkeley.edu>
2020-07-02 14:47:05 -0700

    this migration is killing me

--- db/migrate/20200701003048_add_sold_on_to_items.rb
+++ db/migrate/20200701003048_add_sold_on_to_items.rb
@@ -19,38 +19,36 @@ VALUES
 
     add_column :items, :sold_on, :datetime
     Item.reset_column_information
-    # Set the sold_on date for CanceledItems to the date of cancellation (= item.updated_at)
-    # Set the sold_on date for all other item types to the order's sold_on
-    # Item.transaction do  # not enough dyno memory to allow luxury of this safeguard...
     offset = 0
+    refunds = {}
     while (1)
-      items =  Item.finalized.offset(offset).limit(1000)
-      offset += 1000
-      refunds_created = 0
+      items =  Item.finalized.offset(offset).limit(500)
+      offset += 500
       break if items.empty?
       items.each do |i|
         case i
         when CanceledItem
-          i.account_code_id ||= i.vouchertype.account_code_id if i.amount != 0
-          i.sold_on = i.order.sold_on
+          if i.amount != 0 && i.account_code_id.blank?
+            i.update_column(:account_code_id, i.vouchertype.account_code_id)
+          end
           cancel_time = i.updated_at
-          # create the Refund transaction
-          ref = RefundedItem.from_cancellation(i)
-          ref.sold_on = cancel_time
-          ref.save(validate: false)
-          i.save(validate: false)
-          i.update_attribute(:updated_at, cancel_time)
-          refunds_created += 1
+          i.update_column(:sold_on, i.order.sold_on)
+          refunds[i.id] = cancel_time
         when RefundedItem
           # do nothing
         else
-          orig_updated_at = i.updated_at
-          i.sold_on = i.order.sold_on
-          i.save(validate: false)
-          i.update_attribute(:updated_at, orig_updated_at)
+          i.update_column(:sold_on, i.order.sold_on)
         end
       end
       print "."
+    end
+    puts "Creating #{refunds.keys.size} refunds"
+    refunds.each_pair do |id, cancel_time|
+      i = Item.find(id)
+      # create the Refund transaction
+      ref = RefundedItem.from_cancellation(i)
+      ref.sold_on = cancel_time
+      ref.save!
     end
     puts "\n#{refunds_created} refund items created"
   end
