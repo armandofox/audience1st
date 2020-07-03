81d53d9481542f6d0ae01920513814e99eb6456f
Armando Fox <fox@cs.berkeley.edu>
2020-07-03 11:02:00 -0700

    ensure one_line_description always takes an optional arg

--- app/models/items/canceled_item.rb
+++ app/models/items/canceled_item.rb
@@ -2,7 +2,7 @@ class CanceledItem < Item
   belongs_to :account_code
   def cancel!(by_whom) ; end # overridden from parent class
 
-  def one_line_description ; attributes['comments'] ; end
+  def one_line_description(suppress_price: false) ; attributes['comments'] ; end
   def description_for_report ; '(CANCELLED)' ; end
   def description_for_audit_txn ; attributes['comments'] ; end
   def comments ; '' ; end
