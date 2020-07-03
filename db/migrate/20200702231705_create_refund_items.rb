87b87bdf7c622adb1562c807a296519fd6411d07
Armando Fox <fox@cs.berkeley.edu>
2020-07-02 17:06:05 -0700

    migration phase 2

--- db/migrate/20200702231705_create_refund_items.rb
+++ db/migrate/20200702231705_create_refund_items.rb
@@ -1,11 +1,16 @@
 class CreateRefundItems < ActiveRecord::Migration
   def change
-    CanceledItem.where('amount > 0').each do |i|
+    cxl = CanceledItem.where('amount > 0')
+    puts "Creating #{cxl.count} refund items"
+    count = 0
+    cxl.each do |i|
       orig_updated_at = i.updated_at
       ref = RefundedItem.from_cancellation(i)
       ref.sold_on = i.updated_at
       ref.save!
       i.update_column(:updated_at, orig_updated_at)
+      count += 1
+      print "." if count % 100 == 0
     end
   end
 end
