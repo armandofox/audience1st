98d4e9a89f64e9fe05d94882ee4199b381f647e0
Armando Fox <fox@cs.berkeley.edu>
2020-07-25 11:52:42 -0700

    on show details/redemptions, display YEAR of start/end sales if different from the showdate itself

--- app/helpers/showdates_helper.rb
+++ app/helpers/showdates_helper.rb
@@ -35,6 +35,8 @@ module ShowdatesHelper
   def time_in_words_relative_to(ed,sd)
     if (sd.month == ed.month) && (sd.day == ed.day) && (sd.year == ed.year)
       ed.strftime("%l:%M%p day of show")
+    elsif sd.year != ed.year
+      ed.to_formatted_s(:showtime_including_year)
     else
       ed.to_formatted_s(:showtime)
     end
