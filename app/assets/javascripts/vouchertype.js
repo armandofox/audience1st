2c4f186cfa47c09188d7ad2d57a4e15b767eea5b
Armando Fox <fox@cs.berkeley.edu>
2020-12-01 17:35:35 -0800

    avoid vouchertype.js code executing on pages other than new vouchertype form

--- app/assets/javascripts/vouchertype.js
+++ app/assets/javascripts/vouchertype.js
@@ -1,14 +1,16 @@
 A1.vouchertype = {
   reset_fields: function() {
     var category = $('#vouchertype_category').val();
-    $('.form-row').show();
-    $('input.' + category).val('0'); // numeric price field(s)
-    $('input.' + category).checked = false;
-    $('.form-row.' + category).hide();
+    $('#vouchertype-form form-row').show();
+    $('#vouchertype-form input.' + category).val('0'); // numeric price field(s)
+    $('#vouchertyoe-form input.' + category).checked = false;
+    $('#vouchertype-form .form-row.' + category).hide();
   },
   setup: function() {
-    A1.vouchertype.reset_fields();
-    $('#vouchertype_category').change(A1.vouchertype.reset_fields);
+    if ($('body#vouchertypes_new').length) {
+      A1.vouchertype.reset_fields();
+      $('#vouchertype_category').change(A1.vouchertype.reset_fields);
+    }
   }
 };
 
