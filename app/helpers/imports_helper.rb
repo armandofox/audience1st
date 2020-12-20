5e9935edc005cde106862f1cf87fec9562effcc3
Armando Fox <fox@cs.berkeley.edu>
2020-12-20 14:09:56 -0800

    remove nested .container from options#index

--- app/helpers/imports_helper.rb
+++
@@ -1,9 +0,0 @@
-module ImportsHelper
-
-  def partial_for(str)
-    str.tableize.singularize << "_help"
-  end
-  def partial_names_for(hsh)
-    hsh.reduce({}) { |h, (key,val)| h.merge(val => partial_for(val)) }
-  end
-end
