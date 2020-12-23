a155240ec157f7a58699a5d91228f9fec733fea0
Armando Fox <fox@cs.berkeley.edu>
2020-12-22 17:43:23 -0800

    remove "hilite" from all table classes and css

--- app/helpers/showdates_helper.rb
+++ app/helpers/showdates_helper.rb
@@ -72,21 +72,4 @@ module ShowdatesHelper
       ed.to_formatted_s(:showtime)
     end
   end
-
-  def day_of_week_checkboxes(prefix)
-    dow = %w[Mon Tue Wed Thu Fri Sat Sun]
-    tag = ''
-    dow.each_with_index do |day,i|
-      idx = (i+1) % 7
-      tag <<
-        (content_tag('span', :class => 'hilite') do
-          check_box_tag(prefix, idx, false,
-            :name => "#{prefix}[]", :id => "#{prefix}_#{idx}") +
-            content_tag('label', day, :for => "#{prefix}_#{idx}") 
-        end)
-    end
-    tag.html_safe
-  end
-
-
 end
