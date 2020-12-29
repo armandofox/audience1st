156143cf5de72c9f00ce718b899e1c6e24046b88
Armando Fox <fox@cs.berkeley.edu>
2020-12-28 18:09:37 -0800

    modify migration to put email template in correctly

--- db/migrate/20201218223848_add_email_template_to_options.rb
+++ db/migrate/20201218223848_add_email_template_to_options.rb
@@ -1,7 +1,7 @@
 class AddEmailTemplateToOptions < ActiveRecord::Migration
   def change
     change_table :options do |t|
-      t.text 'html_email_template', :null => false, :default => Mailer::MINIMAL_TEMPLATE
+      t.text 'html_email_template', :null => false, :default => ERB.new(IO.read(File.join(Rails.root, 'app', 'views', 'mailer', 'default_template.html.erb'))).result(binding)
     end
   end
 end
