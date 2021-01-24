9a538d4db78b8e3466527dc8a5c5a0802899b574
Armando Fox <fox@cs.berkeley.edu>
2021-01-24 12:33:07 -0800

    fix factory for reserved-seating showdates

--- spec/factories/shows_and_showdates.rb
+++ spec/factories/shows_and_showdates.rb
@@ -1,36 +1,6 @@
 FactoryBot.define do
 
-  factory :showdate do
-    transient do
-      date { 1.day.from_now.change(:hour => 20, :min => 0) } # tomorrow at 8p
-      show_name "Show"
-    end
-    house_capacity 200
-    thedate { date }
-    show { create(:show, :name => show_name, :including => date) }
-    max_advance_sales { [100, house_capacity].min }
-    live_stream false
-    stream_anytime false
-    factory :reserved_seating_showdate do
-      seatmap { create(:seatmap) }
-    end
-
-    factory :live_stream_showdate do
-      live_stream true
-      house_capacity ValidVoucher::INFINITE
-      access_instructions 'Instructions here for live stream access'
-    end
-    factory :stream_anytime_showdate do
-      stream_anytime true
-      house_capacity ValidVoucher::INFINITE
-      access_instructions 'Instructions here for stream-anytime access'
-    end
-  end
-
   factory :show do
-    transient do
-      including { Time.current }
-    end
     sequence(:name) { |n| "Show #{n}" }
     listing_date { Time.current }
     season { Time.this_season }
@@ -48,4 +18,36 @@ FactoryBot.define do
       s.parse_rows
     end
   end
+
+  factory :showdate do
+    transient do
+      date { 1.day.from_now.change(:hour => 20, :min => 0) } # tomorrow at 8p
+      show_name "Show"
+    end
+    show { FactoryBot.create(:show, :name => show_name) }
+    house_capacity 200
+    thedate { date }
+    max_advance_sales { [100, house_capacity].min }
+    live_stream false
+    stream_anytime false
+    factory :reserved_seating_showdate do
+      transient do
+        sm { FactoryBot.create(:seatmap) }
+      end
+      seatmap { sm }
+      max_advance_sales { sm.seat_count }
+    end
+    
+    factory :live_stream_showdate do
+      live_stream true
+      house_capacity ValidVoucher::INFINITE
+      access_instructions 'Instructions here for live stream access'
+    end
+    factory :stream_anytime_showdate do
+      stream_anytime true
+      house_capacity ValidVoucher::INFINITE
+      access_instructions 'Instructions here for stream-anytime access'
+    end
+  end
+
 end
