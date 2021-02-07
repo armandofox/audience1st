4bd58a6af0aa6523ff6422d9c2e9f70ff6fae178
Armando Fox <fox@cs.berkeley.edu>
2021-02-07 14:28:05 -0800

    remove obsolete migrations

--- db/migrate/20201218223848_add_email_template_to_options.rb
+++
@@ -1,7 +0,0 @@
-class AddEmailTemplateToOptions < ActiveRecord::Migration
-  def change
-    change_table :options do |t|
-      t.text 'html_email_template', :null => false, :default => ERB.new(IO.read(File.join(Rails.root, 'app', 'views', 'mailer', 'default_template.html.erb'))).result(binding)
-    end
-  end
-end
