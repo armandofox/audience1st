0da5b753529a2015f1e1d36733ef62cbee4b77eb
kkhus5 <kkhus5@berkeley.edu>
2021-04-18 15:07:05 -0700

    [chore] remove duplicate column from shows table

--- app/models/mailchimp_test.rb
+++
@@ -1,9 +0,0 @@
-class MailchimpTest
-  require "MailchimpMarketing"
-
-  mailchimp = MailchimpMarketing::Client.new
-  mailchimp.set_config({
-  	:api_key => ENV["b17730e9d2edfef89ae9db104dfc2ebe-us1"],
-  	:server => ENV["us1"]
-  })
-end
