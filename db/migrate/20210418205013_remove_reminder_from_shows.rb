057e6445bad1265ffdf7ada88446506e40873dbb
Justin Wong amazon linux 2 workspace <justinryanwong@berkeley.edu>
2021-04-26 08:36:08 -0700

    fix duplicate row reminder_type in shows migration issue

--- db/migrate/20210418205013_remove_reminder_from_shows.rb
+++ db/migrate/20210418205013_remove_reminder_from_shows.rb
@@ -1,5 +1,5 @@
 class RemoveReminderFromShows < ActiveRecord::Migration
   def change
-  	remove_column :shows, :reminder_type, :string, :limit => 255, :default => "Never", :null => false
+  	remove_column :shows, :reminder_type
   end
 end
