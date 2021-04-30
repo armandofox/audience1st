6df8d6c55fd523d8ab20bffff6236436a72ac9ea
Armando Fox <fox@cs.berkeley.edu>
2021-04-30 10:27:14 -0700

    merge UI changes for show reminder and manual bug patches

--- db/migrate/20210415011621_add_reminder_to_shows.rb
+++
@@ -1,5 +0,0 @@
-class AddReminderToShows < ActiveRecord::Migration
-  def change
-    add_column :shows, :reminder_type, :string, :null => false, :default => Show::REMINDERS.first
-  end
-end
--- db/migrate/20210418205013_remove_reminder_from_shows.rb
+++
@@ -1,5 +0,0 @@
-class RemoveReminderFromShows < ActiveRecord::Migration
-  def change
-  	remove_column :shows, :reminder_type, :string, :limit => 255, :default => "Never", :null => false
-  end
-end
