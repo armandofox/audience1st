1392bf84b14ab0823641a2fb26b286dd00784c33
Armando Fox <fox@cs.berkeley.edu>
2021-07-24 20:29:33 -0700

    simple validations for seating zone name and shortname

--- app/models/seating_zone.rb
+++ app/models/seating_zone.rb
@@ -1,7 +1,10 @@
 class SeatingZone < ActiveRecord::Base
 
-  validates :name, :presence => true, :uniqueness => true
-  validates :short_name, :presence => true, :uniqueness => true
+  validates :name, :presence => true, :uniqueness => true,
+            :format => {:with => /\A[^-:]+\z/, :message => "must not include '-' or ':'"}
+
+  validates :short_name, :presence => true, :uniqueness => true, :length => {:maximum => 8},
+            :format => {:with => /\A[A-Za-z0-9]+\z/, :message => 'only letters and digits allowed'}
 
   has_many :vouchertypes
 
