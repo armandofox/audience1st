2b7c881cddb22036fa7b49a911c6b98cfe890409
Armando Fox <fox@cs.berkeley.edu>
2021-08-01 18:23:11 -0700

    removed unused migrations

--- db/migrate/20210110203219_remove_run_dates_from_show.rb
+++
@@ -1,6 +0,0 @@
-class RemoveRunDatesFromShow < ActiveRecord::Migration
-  def change
-    remove_column 'shows', 'opening_date'
-    remove_column 'shows', 'closing_date'
-  end
-end
--- db/migrate/20210415014347_add_reminder_to_options.rb
+++
@@ -1,5 +0,0 @@
-class AddReminderToOptions < ActiveRecord::Migration
-  def change
-  	add_column :options, :reminder_emails, :string, :default => "Never"
-  end
-end
