8f835d875a4027dd66c704dff9a23f022a4f39af
Armando Fox <fox@cs.berkeley.edu>
2021-09-08 18:48:05 -0700

    last repair to vouchertype menu fields

--- app/assets/javascripts/vouchertype.js
+++ app/assets/javascripts/vouchertype.js
@@ -1,6 +1,6 @@
 A1.vouchertype = {
   reset_fields: function() {
-    var category = $('#vouchertype_category').val();
+    var category = $('.vouchertype-category').val();
     $('#vouchertype-form .form-row').show();
     $('#vouchertype-form .form-row.' + category).hide();
     $('#vouchertype-form input.' + category).val('0'); // numeric price field(s)
@@ -16,7 +16,7 @@ A1.vouchertype = {
   },
   setup: function() {
     $('.vouchertype-filter').change(A1.vouchertype.filter_this_vouchertype);   
-    if ($('body#vouchertypes_new').length) {
+    if ($('body#vouchertypes_new').length || $('body#vouchertypes_edit').length) {
       A1.vouchertype.reset_fields();
       $('#vouchertype_category').change(A1.vouchertype.reset_fields);
     }
