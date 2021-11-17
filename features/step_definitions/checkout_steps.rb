dc82e50a83be484a3a90137078ecb26073ae460a
Armando Fox <fox@cs.berkeley.edu>
2021-11-17 10:31:47 -0800

    Now generate conf email for quick_donation flow, but required fixing the Customer.for_donation function to account for cases where we know the (non-logged-in) customer but the billing address they enter is different from what's on file.  Expanded the quick_donation feature to also check for email sending and correct address updating, and a spec for Customer.for_donation.  Address matching etc really needs to be factored into its own class.

--- features/step_definitions/checkout_steps.rb
+++ features/step_definitions/checkout_steps.rb
@@ -1,6 +1,6 @@
 # Fill in fields for gift, checkout, etc
 
-When /^I fill in the "(.*)" fields with "(\S+)\s+(\S+),\s*([^,]+),\s*([^,]+),\s*(\S+)\s+(\S+),\s*([^,]+),\s*(.*@.*)"$/ do |fieldset, first, last, street, city, state, zip, phone, email|
+When /^I fill in the "(.*)" fields with "(\S+)\s+(\S+),\s*([^,]+),\s*([^,]+),\s*(\S+)\s+(\S+),\s*([^,]+),\s*(.*)"$/ do |fieldset, first, last, street, city, state, zip, phone, email|
   with_scope fieldset do
     fill_in 'customer[first_name]', :with => first
     fill_in 'customer[last_name]', :with => last
