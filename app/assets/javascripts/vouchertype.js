a5b7dd031feaba1af6df186ad53521c6c80d8006
Armando Fox <fox@cs.berkeley.edu>
2021-12-24 10:29:12 -0800

    vouchertype field hiding/showing when editing vouchertype new works properly after cloning; some dead code also removed from vouchertype.js

--- app/assets/javascripts/vouchertype.js
+++ app/assets/javascripts/vouchertype.js
@@ -7,17 +7,8 @@ A1.vouchertype = {
     $('#vouchertype-form input.' + category).val('0'); // numeric price field(s)
     $('#vouchertype-form input.' + category).checked = false;
   },
-  filter_this_vouchertype: function() {
-    var klass = 'tr.' + $(this).attr('name'); // eg 'tr.revenue'
-    if ($(this).is(':checked')) {
-      $(klass).show();
-    }  else {
-      $(klass).hide();
-    }
-  },
   setup: function() {
-    $('.vouchertype-filter').change(A1.vouchertype.filter_this_vouchertype);   
-    if ($('body#vouchertypes_new').length || $('body#vouchertypes_edit').length) {
+    if ($('#vouchertype-form').length) {
       A1.vouchertype.reset_fields();
       $('#vouchertype_category').change(A1.vouchertype.reset_fields);
     }
