fdde3d62b59b07448c67fbdb80e5ee9f173d7441
Armando Fox <fox@cs.berkeley.edu>
2022-06-09 12:58:47 -0700

    move newrelic gem to all groups to allow testing; basic specs for noticing stale orders

--- app/services/stale_order_sweeper.rb
+++ app/services/stale_order_sweeper.rb
@@ -5,9 +5,13 @@ class StaleOrderSweeper
   # There's only one class method, designed to be called from a periodic runner or similar.
 
   def self.sweep!
-    # destroy stale orders and update sweep timer
-    stale_order_date = (Option.order_timeout + 1).minutes.ago # just to be on the safe side
-    stale_import_date = (Option.import_timeout + 1).minutes.ago
+    # raise error for orders where CC charge & order proc didn't happen atomically
+    self.notice_failed_orders!
+    # destroy stale (customer-abandoned) orders and update sweep timer
+    self.sweep_abandoned_orders_and_imports
+  end
+
+  def self.notice_failed_orders!
     # first check for 'pending' CC orders and raise an alarm if any are found, since no
     # order should ever be 'pending' for more than a few seconds
     if (pending = Order.pending_but_paid.abandoned_since(2.minutes.ago))
@@ -17,10 +21,16 @@ class StaleOrderSweeper
       # mark those order(s) as errored, so they don't trigger the warning again
       pending.update_all(:authorization => Order::ERRORED)
     end
+  end
+
+  def self.sweep_abandoned_orders_and_imports
+    stale_order_date = (Option.order_timeout + 1).minutes.ago # just to be on the safe side
+    stale_import_date = (Option.import_timeout + 1).minutes.ago
     ActiveRecord::Base.transaction do
       Order.where(:type => 'Order').abandoned_since(stale_order_date).destroy_all
       TicketSalesImport.abandoned_since(stale_import_date).destroy_all
       Option.first.update_attribute(:last_sweep, Time.current)
     end
   end
+
 end
