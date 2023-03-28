be17d610d0a92063f6d363ce99dcca1bef0a2748
Armando Fox <fox@cs.berkeley.edu>
2023-03-27 18:57:36 -0700

    updating to ruby 2.7.7 for heroku-20

--- features/support/webmock.rb
+++ features/support/webmock.rb
@@ -1,5 +1,6 @@
 require 'webmock/cucumber'
 WebMock.enable!
 WebMock.disable_net_connect!(
+  net_http_connect_on_start: true, # @see https://github.com/bblimke/webmock/blob/master/README.md#connecting-on-nethttpstart
   allow_localhost: true,
   allow: "chromedriver.storage.googleapis.com")
