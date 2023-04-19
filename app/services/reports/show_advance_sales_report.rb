54cc173b34ab4dfac2e36f4d46e6ed5e9ddea1ad
Armando Fox <fox@cs.berkeley.edu>
2023-04-19 10:05:55 -0700

    only show checked in count on first line of each showdate in advance sales download

--- app/services/reports/show_advance_sales_report.rb
+++ app/services/reports/show_advance_sales_report.rb
@@ -29,6 +29,7 @@ class ShowAdvanceSalesReport
         show.showdates.each do |sd|
           vouchers = sd.vouchers.finalized
           sales = Showdate::Sales.new(vouchers.group_by(&:vouchertype), sd.revenue_per_seat, sd.total_offered_for_sale)
+          first_row = true
           sales.vouchers.each_pair do |vt,v|
             max_sales = vt.valid_vouchers.find { |v| v.showdate_id == sd.id }.max_sales_for_type
             csv << [
@@ -37,7 +38,7 @@ class ShowAdvanceSalesReport
               sd.thedate,
               sd.house_capacity,
               sd.max_advance_sales,
-              sd.num_checked_in,
+              (first_row ? sd.num_checked_in : ''),
               vt.name,
               (if vt.subscriber_voucher? then "YES" else "" end),
               (if max_sales != ValidVoucher::INFINITE then max_sales else "" end),
@@ -45,6 +46,7 @@ class ShowAdvanceSalesReport
               (sprintf "%.02f", vt.price),
               (sprintf "%.02f", vt.price * v.size)
             ]
+            first_row = false
           end
         end
       end
