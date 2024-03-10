609c820439bd52a2cbc5a095dec361666a7a0652
Winson Wan <winsontsangw@gmail.com>
2024-03-10 04:00:05 -0700

    add method to create new record from account_code and overwrite 'must overwrite' methods

--- app/models/items/recurring_donation.rb
+++ app/models/items/recurring_donation.rb
@@ -7,7 +7,10 @@ class RecurringDonation < Item
   validates_associated :account_code
   validates_presence_of :account_code_id
 
-  validates_numericality_of :amount
-  validates_inclusion_of :amount, :in => 1..10_000_000, :message => "must be at least 1 dollar"
+  def self.from_account_code(account_code)
+    RecurringDonation.new(:amount => 0, :account_code => account_code)
+  end
 
+  def one_line_description ; end
+  def description_for_audit_txn ; end
 end
