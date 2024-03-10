cc6dbc1682ab358a68c996250860153510f2471a
Winson Tsang Wan <106371049+winsonwan@users.noreply.github.com>
2024-03-09 22:08:16 -0800

    Revert "Create recurring donations model + Update controller action to create new recurring donation instance "

--- app/models/items/donation.rb
+++ app/models/items/donation.rb
@@ -6,15 +6,13 @@ class Donation < Item
   def self.default_code
     AccountCode.find(Option.default_donation_account_code)
   end
-
+  
   belongs_to :account_code
   validates_associated :account_code
   validates_presence_of :account_code_id
-
+  
   belongs_to :customer
-
-  belongs_to :recurring_donation
-
+  
   validates_numericality_of :amount
   validates_inclusion_of :amount, :in => 1..10_000_000, :message => "must be at least 1 dollar"
 
--- spec/factories/recurring_donations.rb
+++
@@ -1,5 +0,0 @@
-FactoryBot.define do
-  factory :recurring_donation do
-    
-  end
-end
