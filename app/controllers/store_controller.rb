4025a192f8555f60fac33bc0677511e116e47cd0
adam2451 <adamlewdong2019@gmail.com>
2024-03-14 22:02:39 -0700

    refactor new session view into two partials

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -115,6 +115,7 @@ class StoreController < ApplicationController
     reset_shopping                 # even if order in progress, going to donation page cancels it
     if @customer == Customer.anonymous_customer
       # handle donation as a 'guest checkout', even though may end up being tied to real customer
+      @head = "Login for a faster checkout!"
       @customer = Customer.new
       session[:guest_checkout] = true 
       return_after_login params.except(:customer_id)
