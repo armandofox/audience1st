e3b8a2a3c561f97b8d8ace74b15ab0f909df2159
Armando Fox <fox@cs.berkeley.edu>
2024-07-22 15:50:08 -0700

    remove spurious dup migration

--- db/migrate/20240225093946_add_recurring_donation_options_to_options_table.rb
+++
@@ -1,9 +0,0 @@
-class AddRecurringDonationOptionsToOptionsTable < ActiveRecord::Migration
-  def change
-    add_column :options, :allow_recurring_donations, :boolean, default: false
-    add_column :options, :default_donation_frequency, :string, default: "One Time"
-    add_column :options, :recurring_donation_contact_emails, :text
-    add_column :options, :notify_theater_about_new_recurring_donation, :boolean, default: true
-    add_column :options, :notify_theater_about_failed_recurring_donation_charge, :boolean, default: true
-  end
-end
