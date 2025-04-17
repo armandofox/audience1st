d30712fcd8dbb30a017cf15d4b27c20a1862a853
Armando Fox <fox@cs.berkeley.edu>
2025-04-17 13:10:58 -0700

    try to get cobertura in ci

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,6 +1,7 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'simplecov'
 SimpleCov.start 'rails'
+SimpleCov.formatter = SimpleCov::Formatter::JSONFormatter
 
 require 'spec_helper'
 require File.expand_path('../../config/environment', __FILE__)
