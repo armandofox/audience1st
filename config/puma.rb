3dd96fc925ddf47b001c1b42ff7ecb3e4cd5f23f
Armando Fox <fox@cs.berkeley.edu>
2025-08-18 15:36:45 -0700

    back off to 1 worker, 1 thread for now

--- config/puma.rb
+++ config/puma.rb
@@ -2,8 +2,11 @@
 # Heroku says "2 to 4" workers should be able to run within the memory
 # footprint of a single dyno.  The values here can be overridden by
 # setting envariables.
-workers Integer(ENV['WEB_CONCURRENCY'] || 2)
-threads_count = Integer(ENV['RAILS_MAX_THREADS'] || 1)
+
+WORKERS_FOR_BASIC_DYNOS = 1
+WORKERS_FOR_STANDARD_DYNOS = 2
+workers WORKERS_FOR_BASIC_DYNOS
+threads_count = 1
 threads threads_count, threads_count
 
 preload_app!
