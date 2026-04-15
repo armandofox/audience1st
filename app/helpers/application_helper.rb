e86e7d10a63b7b9468813c8cd7f7104bd5061725
Armando Fox <fox@cs.berkeley.edu>
2026-04-14 19:12:33 -0700

    fix css inclusion

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -11,10 +11,6 @@ module ApplicationHelper
     end
   end
 
-
-    tag('link', {rel: "stylesheet", href: "https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css", integrity: "sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO", crossorigin: "anonymous"})
-  end
-
   def venue_stylesheet_link_tag
     if (url = Option.stylesheet_url).blank?
       return unless Rails.env.development?
