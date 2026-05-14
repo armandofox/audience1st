8fc5f56086c1df025031765854e4f50bb40a31b1
Armando Fox <fox@cs.berkeley.edu>
2026-05-14 10:31:09 -0700

    fixes #184

---
+++ db/migrate/20260514172713_add_database_constraint_unique_seat.rb
@@ -0,0 +1,13 @@
+class AddDatabaseConstraintUniqueSeat < ActiveRecord::Migration[6.1]
+  # issue #183: add DB constraint to ensure unique seats
+  def up
+    add_index :items, [:showdate_id, :seat],
+              unique: true,
+              where: "finalized = TRUE AND seat IS NOT NULL",
+              name: "items_finalized_showdate_seat_unique"
+  end
+
+  def down
+    remove_index :items, name: "items_finalized_showdate_seat_unique"
+  end
+end
